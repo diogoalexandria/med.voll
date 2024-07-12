116b4acca9ee7bd78a1bc6d7b6cd2d360c19de0e
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2024-07-12 15:45:54 -0300

    feat: Criando classe controller de médicos e records de cadastro de médicos

---
+++ src/main/java/med/voll/api/medico/CadastroMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record CadastroMedico(
+    String nome,
+    String email,
+    String crm,
+    Especialidade especialidade, 
+    Endereco endereco
+) {
+    
+}
