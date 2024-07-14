2d50b04fa4129b94c06788d35cad6bb373e3c8b4
Diogo Alexandria <diogo.alexandria@cnseg.org.br>
2024-07-14 17:58:23 -0300

    refact: Refatorando nome da classe DTO de endereço

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,11 +1,33 @@
 package med.voll.api.endereco;
 
-public record Endereco(
-    String logradouro,
-    String bairro,
-    String cep,
-    String cidade,
-    String uf,
-    String complemento,
-    String numero
-) {}
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Embeddable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+    
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+    public Endereco(EnderecoDTO endereco) {
+        this.logradouro  = endereco.logradouro();
+        this.bairro      = endereco.bairro();
+        this.cep         = endereco.cep();
+        this.cidade      = endereco.cidade();
+        this.uf          = endereco.uf();
+        this.complemento = endereco.complemento();
+        this.numero      = endereco.numero();
+    }
+
+
+}
