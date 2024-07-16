6a41def70ae4fb358b2ec0fa8dc7cbdda066385d
Diogo Alexandria <diogo.alexandria@cnseg.org.br>
2024-07-16 11:27:42 -0300

    feat: Inserindo validações dos campos para cadstro de paciente

--- src/main/java/med/voll/api/paciente/PacienteDTO.java
+++ src/main/java/med/voll/api/paciente/PacienteDTO.java
@@ -1,11 +1,30 @@
 package med.voll.api.paciente;
 
 import med.voll.api.endereco.EnderecoDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PacienteDTO(
+
+    @NotBlank
     String nome,
+
+    @NotBlank
+    @Email     
     String email,
+
+    @NotBlank
     String telefone,
+
+    @NotBlank
+    @Pattern(regexp = "//d{}")
     String cpf,
+    
+    @NotNull
+    @Valid
     EnderecoDTO endereco
+    
 ) {} 
