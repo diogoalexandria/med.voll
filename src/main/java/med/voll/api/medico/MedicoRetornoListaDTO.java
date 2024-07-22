724b65bd6fd36918527689d35f2342312bfdbec5
Diogo Alexandria <diogo.alexandria@cnseg.org.br>
2024-07-22 19:28:41 -0300

    feat: Retornando id na listagem de médicos

--- src/main/java/med/voll/api/medico/MedicoRetornoListaDTO.java
+++ src/main/java/med/voll/api/medico/MedicoRetornoListaDTO.java
@@ -1,13 +1,21 @@
 package med.voll.api.medico;
 
 public record MedicoRetornoListaDTO(
+
+    Long id,
+
     String nome,
+
     String email,
+
     String crm,
+
     MedicoEspecialidadesEnum especialidade
+    
 ) {
     public MedicoRetornoListaDTO (Medico medico) {
         this(
+            medico.getId(),
             medico.getNome(),
             medico.getEmail(),
             medico.getCrm(),
